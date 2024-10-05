c32154c88c430de2416aabeb34c0a1c1441ebac8
Patrick Bucher <patrick.bucher@mailbox.org>
2024-10-05 08:20:52 +0200

    chapter 6: exercise 3

--- 1-javascript/eloquent-javascript/ch06/group.js
+++ 1-javascript/eloquent-javascript/ch06/group.js
@@ -37,12 +37,34 @@ class Group {
     return false;
   }
 
+  [Symbol.iterator]() {
+    return new GroupIterator(this.#items);
+  }
+
   static fromIterable(iter) {
     const group = new Group();
     for (const item of iter) {
       group.add(item);
     }
     return group;
+  }
+}
+
+class GroupIterator {
+  #items;
+  #i;
+
+  constructor(items) {
+    this.#items = items;
+    this.#i = -1;
+  }
+
+  next() {
+    this.#i++;
+    if (this.#i < this.#items.length) {
+      return { value: this.#items[this.#i], done: false };
+    }
+    return { done: true };
   }
 }
 
@@ -53,3 +75,7 @@ console.log(primes.has(1));
 primes.add(11);
 primes.add(11);
 console.log(primes.items);
+
+for (const prime of primes) {
+  console.log(prime);
+}
