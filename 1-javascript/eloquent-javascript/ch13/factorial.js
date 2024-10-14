c9418f001f6262951b6e667b62c4a0893b488da9
Patrick Bucher <patrick.bucher@mailbox.org>
2024-10-14 07:07:12 +0200

    chapter 13: fixed example

--- 1-javascript/eloquent-javascript/ch13/factorial.js
+++ 1-javascript/eloquent-javascript/ch13/factorial.js
@@ -1,7 +1,7 @@
 export function factorial(n) {
   if (n < 0) {
     throw new Error(`cannot compute factorial of negative number ${n}`);
-  } else if (n == 0} {
+  } else if (n == 0) {
     return 1;
   } else {
     return n * factorial(n - 1);
