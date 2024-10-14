899668d5c4c7fb44c72bebfaa14c75f8c26170b7
Patrick Bucher <patrick.bucher@mailbox.org>
2024-10-14 07:03:07 +0200

    chapter 13: load modules (fails)

---
+++ 1-javascript/eloquent-javascript/ch13/demo.js
@@ -0,0 +1,4 @@
+import { factorial } from "factorial.js";
+
+let number = prompt("enter a positive number");
+console.log(factorial(Number.parseInt(number)));
---
+++ 1-javascript/eloquent-javascript/ch13/factorial.js
@@ -0,0 +1,9 @@
+export function factorial(n) {
+  if (n < 0) {
+    throw new Error(`cannot compute factorial of negative number ${n}`);
+  } else if (n == 0} {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
