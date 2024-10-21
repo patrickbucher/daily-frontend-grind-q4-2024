0e8efabfd388c06b0c469f8a7f65b0bb56adbeb1
Patrick Bucher <patrick.bucher@mailbox.org>
2024-10-21 07:02:12 +0200

    started implementing repetition calculator

---
+++ 1-javascript/eloquent-javascript/ch18/reps.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", (_) => {
+  const form = document.getElementById("reps-form");
+  const submit = form.elements.submit;
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // TODO: calculate
+  });
+});
