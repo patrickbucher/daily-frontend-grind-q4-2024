2ca3a6d53217af1de27b7387bcf3d86be0e6019c
Patrick Bucher <patrick.bucher@mailbox.org>
2024-10-24 07:13:12 +0200

    chapter 19: continued

--- 1-javascript/eloquent-javascript/ch19/painting.js
+++ 1-javascript/eloquent-javascript/ch19/painting.js
@@ -16,9 +16,28 @@ class Picture {
 
   draw(pixels) {
     let copy = this.pixels.slice();
-    for (let {x, y, color} of pixels) {
+    for (let { x, y, color } of pixels) {
       copy[x + y * this.width] = color;
     }
     return new Picture(this.width, this.height, copy);
   }
+}
+
+function updateState(state, action) {
+  return { ...state, ...action };
+}
+
+function elt(type, props, ...children) {
+  let dom = document.createElement(type);
+  if (props) {
+    Object.assign(dom, props);
+    for (let child of children) {
+      if (typeof child != "string") {
+        dom.appendChild(child);
+      } else {
+        dom.appendChild(document.createTetNode(child));
+      }
+    }
+  }
+  return dom;
 }
