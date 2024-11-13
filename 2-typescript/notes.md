29b919a6f15413c1fcbd68db3035774e946cc577
Patrick Bucher <patrick.bucher@mailbox.org>
2024-11-13 06:27:29 +0100

    iterator example

--- 2-typescript/notes.md
+++ 2-typescript/notes.md
@@ -1470,3 +1470,49 @@ Output:
 
 Notice that for `bob` the `toString` method of `Employee` is called, even though
 the static `output` method is defined on `Person`.
+
+### Iterators
+
+An iterator provides a function called `next`, which returns a sequence of
+objects containing a `value` and a `done` property, the latter indicating
+whether or not the sequence has been exhausted:
+
+```javascript
+class Sequence {
+  constructor(step, n) {
+    this.step = step;
+    this.n = n;
+    this.value = 0;
+    this.i = 0;
+  }
+
+  next() {
+    this.value += this.step;
+    this.i++;
+    return {
+      value: this.value,
+      done: this.i > this.n,
+    };
+  }
+}
+
+let rowOfSix = new Sequence(6, 10);
+let result = rowOfSix.next();
+while (!result.done) {
+  console.log(result.value);
+  result = rowOfSix.next();
+}
+```
+
+Output:
+
+    6
+    12
+    18
+    24
+    30
+    36
+    42
+    48
+    54
+    60
--- 2-typescript/primer/index.js
+++ 2-typescript/primer/index.js
@@ -1,34 +1,24 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+class Sequence {
+  constructor(step, n) {
+    this.step = step;
+    this.n = n;
+    this.value = 0;
+    this.i = 0;
   }
 
-  toString() {
-    return `${this.name} is ${this.age} years old`;
-  }
-
-  static output(...people) {
-    people.forEach((p) => console.log(p.toString()));
-  }
-}
-
-class Employee extends Person {
-  constructor(name, age, percentage, salary) {
-    super(name, age);
-    this.percentage = percentage;
-    this.salary = salary;
-  }
-
-  toString() {
-    return `${super.toString()} and earns ${this.#salary()}`;
-  }
-
-  #salary() {
-    return this.percentage * (this.salary / 100.0);
+  next() {
+    this.value += this.step;
+    this.i++;
+    return {
+      value: this.value,
+      done: this.i > this.n,
+    };
   }
 }
 
-let alice = new Person("Alice", 52);
-let bob = new Employee("Bob", 47, 90, 120000);
-Person.output(alice, bob);
+let rowOfSix = new Sequence(6, 10);
+let result = rowOfSix.next();
+while (!result.done) {
+  console.log(result.value);
+  result = rowOfSix.next();
+}
