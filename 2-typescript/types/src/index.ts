6222dbf902e040b4114dcc6e8a4c24da5e45c6b8
Patrick Bucher <patrick.bucher@mailbox.org>
2024-12-09 07:23:00 +0100

    [WIP]: interface example

--- 2-typescript/types/src/index.ts
+++ 2-typescript/types/src/index.ts
@@ -1,41 +1,71 @@
-abstract class Animal {
-  constructor(public name: string) {}
+interface Shape {
+  circumference(): number;
+  area(): number;
+}
+
+interface Named {
+  name: string;
+}
+
+interface Describable {
+  describe(): string;
+}
+
+abstract class Rectangular implements Shape, Named {
+  public name: string;
+
+  abstract circumference(): number;
+  abstract area(): number;
+}
+
+class Square extends Rectangular {
+  constructor(public side: number) {
+    super();
+  }
+
+  circumference(): number {
+    return 4 * this.side;
+  }
+
+  area(): number {
+    return this.side * this.side;
+  }
+}
+
+class Rectangle extends Square implements Describable {
+  constructor(
+    side: number,
+    public otherSide: number,
+  ) {
+    super(side);
+    this.name = "Rectangle";
+  }
+
+  circumference(): number {
+    return 2 * this.side + 2 * this.otherSide;
+  }
+
+  area(): number {
+    return this.side * this.otherSide;
+  }
 
   describe(): string {
-    return `I'm ${this.name} the ${this.species()} and I make «${this.noise()}».`;
-  }
-
-  abstract species(): string;
-  abstract noise(): string;
-}
-
-class Horse extends Animal {
-  constructor(name: string) {
-    super(name);
-  }
-
-  species(): string {
-    return "Horse";
-  }
-
-  noise(): string {
-    return "Neigh";
+    return `${this.name} of ${this.side}x${this.otherSide}`;
   }
 }
 
-class Cat extends Animal {
-  constructor(name: string) {
-    super(name);
-  }
+let shapes: Shape[] = [new Rectangle(3, 4), new Square(5)];
 
-  species(): string {
-    return "Cat";
+shapes.forEach((s) => {
+  let description: string;
+  if ("name" in s) {
+    description = s.name as string;
+  } else if ("describe" in s && typeof s.describe == "function") {
+    description = s.describe();
+  } else {
+    description = "Unknown shape";
   }
-
-  noise(): string {
-    return "Meow";
-  }
-}
-
-let animals: Animal[] = [new Horse("Betty"), new Cat("Dimka")];
-animals.forEach((a) => console.log(a.describe()));
+  console.log(
+    `${description} with circumference of ${s.circumference()} and area of ${s.area()}`,
+  );
+});
