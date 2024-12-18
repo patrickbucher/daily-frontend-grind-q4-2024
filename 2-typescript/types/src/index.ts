51152624594412ff71efdb3cbd65a2f8eb42b817
Patrick Bucher <patrick.bucher@mailbox.org>
2024-12-18 06:59:14 +0100

    cleanup

--- 2-typescript/types/src/index.ts
+++ 2-typescript/types/src/index.ts
@@ -1,35 +1,5 @@
-class Collection<T> {
-  private items: T[] = [];
-
-  add(item: T) {
-    this.items.push(item);
-  }
-
-  filter<V extends T>(predicate: (target: T) => target is V): V[] {
-    return this.items.filter((i) => predicate(i)) as V[];
-  }
+function greet(whom: string): string {
+  return `Hello, ${whom}!`;
 }
 
-class Car {
-  constructor(
-    public brand: string,
-    public model: string,
-  ) {}
-}
-
-class Drink {
-  constructor(
-    public brand: string,
-    public name: string,
-  ) {}
-}
-
-function isDrink(target: any): target is Drink {
-  return target instanceof Drink;
-}
-
-let stuff = new Collection<Car | Drink>();
-stuff.add(new Car("Porsche", "911"));
-stuff.add(new Drink("Jack Daniels", "Whiskey"));
-let drinks = stuff.filter<Drink>(isDrink);
-console.log(drinks);
+console.log(greet("Joe"));
